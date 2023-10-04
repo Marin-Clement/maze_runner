4a014f35ac67f28cf2b23ecb597fd73a0920e053
clementmarin <clement.marin@laplateforme.io>
2023-10-04 22:55:54 +0200

    [ADD] GraphBasedMazeGenerator

---
+++ src/main/java/MazeRunner.java
@@ -0,0 +1,48 @@
+import maze.Objects.Maze;
+
+public class MazeRunner {
+    public static void main(String[] args) {
+        try {
+            if (args.length != 4) {
+                howToUse();
+                return;
+            }
+
+            int width = Integer.parseInt(args[0]);
+            int height = Integer.parseInt(args[1]);
+            String generationType = args[2];
+            String generatorMethod = args[3];
+
+            if (width < 5 || height < 5) {
+                System.err.println("Erreur : Veuillez fournir une largeur et une hauteur valides supérieurs à 5");
+                howToUse();
+                return;
+            }
+
+            if (!isValidGenerationType(generationType) || !isValidGeneratorMethod(generatorMethod)) {
+                System.err.println("Erreur : Veuillez fournir un type de labyrinthe et une méthode de génération valides.");
+                howToUse();
+                return;
+            }
+
+            new Maze(width, height, generationType, generatorMethod);
+        } catch (NumberFormatException e) {
+            System.err.println("Erreur : Veuillez fournir une largeur et une hauteur valides supérieurs à 5");
+            howToUse();
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException | OutOfMemoryError e) {
+            System.err.println("Erreur inattendue lors de la génération du labyrinthe. Veuillez réessayer.");
+        }
+    }
+
+    private static void howToUse() {
+        System.err.println("Utilisation : java -jar MazeRunner.jar [largeur] [hauteur] [perfect/imperfect] [simple/graph/optimized]");
+    }
+
+    private static boolean isValidGenerationType(String type) {
+        return type.equals("perfect") || type.equals("imperfect");
+    }
+
+    private static boolean isValidGeneratorMethod(String method) {
+        return method.equals("simple") || method.equals("graph") || method.equals("optimized");
+    }
+}
---
+++ src/main/java/maze/generators/GraphBasedMazeGenerator.java
@@ -0,0 +1,83 @@
+package maze.generators;
+
+import maze.Objects.Cell;
+import maze.MazeGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.Random;
+
+public class GraphBasedMazeGenerator implements MazeGenerator {
+
+    private final Random random = new Random();
+
+    public GraphBasedMazeGenerator(Cell[] cell, int width, int height, boolean perfect) {
+        generateMaze(cell, width, height, perfect);
+    }
+
+    @Override
+    public void generateMaze(Cell[] cell, int width, int height, boolean perfect) {
+        List<Integer> possibilities = new ArrayList<>(4);
+        Stack<Integer> path = new Stack<>();
+        path.push(0);
+
+        while (!path.isEmpty()) {
+            possibilities.clear();
+            int id = path.peek();
+            cell[id].setVisited();
+            int x = id % width;
+            int y = id / width;
+
+            if (y != 0 && cell[id - width].isVisited()) {
+                possibilities.add(0);
+            }
+            if (x != width - 1 && cell[id + 1].isVisited()) {
+                possibilities.add(1);
+            }
+            if (y != height - 1 && cell[id + width].isVisited()) {
+                possibilities.add(2);
+            }
+            if (x != 0 && cell[id - 1].isVisited()) {
+                possibilities.add(3);
+            }
+
+            if (!possibilities.isEmpty()) {
+                int direction = random.nextInt(possibilities.size());
+                switch (possibilities.get(direction)) {
+                    case 0 -> {
+                        cell[id].removeWall(0);
+                        cell[id - width].removeWall(2);
+                        path.push(id - width);
+                    }
+                    case 1 -> {
+                        cell[id].removeWall(1);
+                        cell[id + 1].removeWall(3);
+                        path.push(id + 1);
+                    }
+                    case 2 -> {
+                        cell[id].removeWall(2);
+                        cell[id + width].removeWall(0);
+                        path.push(id + width);
+                    }
+                    case 3 -> {
+                        cell[id].removeWall(3);
+                        cell[id - 1].removeWall(1);
+                        path.push(id - 1);
+                    }
+                }
+            } else {
+                path.pop();
+            }
+        }
+        if (!perfect) {
+            if (cell[0].hasWall(1)) {
+                cell[0].removeWall(1);
+                cell[1].removeWall(3);
+            } else {
+                cell[0].removeWall(2);
+                cell[width].removeWall(0);
+            }
+        }
+    }
+}
