0b554b8a95d09439a9227c30b50d14bbf61d18ab
clementmarin <clement.marin@laplateforme.io>
2023-10-04 22:56:56 +0200

    [ADD] OptimizedMazeGenerator (No Implemented)

---
+++ src/main/java/maze/generators/OptimizedMazeGenerator.java
@@ -0,0 +1,16 @@
+package maze.generators;
+
+import maze.Objects.Cell;
+import maze.MazeGenerator;
+
+public class OptimizedMazeGenerator implements MazeGenerator {
+
+    public OptimizedMazeGenerator(Cell[] cell, int width, int height, boolean perfect) {
+        generateMaze(cell, width, height, perfect);
+    }
+
+    @Override
+    public void generateMaze(Cell[] cell, int width, int height, boolean perfect) {
+        System.out.println("Not implemented :( But have a nice day though :)");
+    }
+}
